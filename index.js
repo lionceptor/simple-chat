c6827d968da0e92ceb44e6a4c0edc3b51562b5bf
lionceptor <31250153+lionceptor@users.noreply.github.com>
2017-12-13 21:34:18 -0600

    simplification of text

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var http = require('http').Server(app);
 var port = 3000;
 var io = require('socket.io')(http);
 
+
+console.log("Hi Mrs. Horning!";)
+
 app.get('/', function(req,res)
 {
 	res.sendFile(__dirname + '/index.html');
@@ -13,8 +16,9 @@ io.on('connection', function(socket)
 {
 	socket.on('chat message', function(text)
 	{
-		console.log('message: ' + text);	
+		console.log('test message: ' + text);	
 	});
 });
 
 http.listen(port);
+console.log("Listening on port" + port);
