e9f33486ef46eb4637aee93425e6c95374d02010
lionceptor <31250153+lionceptor@users.noreply.github.com>
2017-12-13 20:54:55 -0600

    removed listen from app.get due to implemetnation of io

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var port = 3000;
+var io = require('socket.io')(http);
 
 app.get('/', function(req,res)
 {
 	res.sendFile(__dirname + '/index.html');
 
-}).listen(port); 
+});
 
 io.on('connection', function(socket)
 {
 	console.log('new connection');
 });
+
+http.listen(port);
